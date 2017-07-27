1cf6ad7bb7cd991b8ad169262e6671d21e1b2b6b
Hao.hung <eeit9306@gmail.com>
2017-07-27 23:09:05 +0800

    輸入後最下面會有固定的欄位

--- todoMVC/WebContent/js/myModule.js
+++ todoMVC/WebContent/js/myModule.js
@@ -3,8 +3,10 @@ var todoAPP = angular.module('TodoAPP',[]);
 todoAPP.controller('Ctrl1',['$scope',function(s){
 	s.myInputs=[];
 	//s.classObj={useItalic: 'italic'};
+	s.inputString='';
 	s.addtodo = function() {
 		s.myInputs.push(s.inputString);
+		s.inputString='';
 	};
 	s.remove = function(index){
 		s.myInputs.splice(index, 1);
