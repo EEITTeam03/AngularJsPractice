e069e94d7ce0c1840ebd8fe6b5cc72064d1107c5
Hao.hung <eeit9306@gmail.com>
2017-07-30 00:00:34 +0800

    加入全選功能

--- todoMVC/WebContent/js/myModule.js
+++ todoMVC/WebContent/js/myModule.js
@@ -1,49 +1,59 @@
-var todoAPP = angular.module('TodoAPP',['ngRoute'],function(
-		$routeProvider) {
-	$routeProvider.when('/:path', {
-		templateUrl : 'view.html',
-		controller:'HideCtrl'
-	}).otherwise({
-		redirectTo : '/'
-	});
-});
+var todoAPP = angular.module('TodoAPP', [ 'ngRoute' ],
+		function($routeProvider) {
+			$routeProvider.when('/:path', {
+				templateUrl : 'view.html',
+				controller : 'HideCtrl'
+			}).otherwise({
+				redirectTo : '/'
+			});
+		});
 
-todoAPP.controller('Ctrl1',['$scope','$routeParams','$filter',function(s,r,f){
-	s.myInputs=[];
-	s.inputString='';
-	//s.count = 0;
-	s.addtodo = function() {
-		s.myInputs.push({text:s.inputString, status:false});
-		s.inputString='';
-	};
-	s.remove = function(index){
-		s.myInputs.splice(index, 1);
-	};
-	
-	//監控myInputs陣列元素的變化,第3個參數為true會呼叫equals方法來比較
-	s.$watch("myInputs", function(newValue, oldValue) {
-		
-		s.count = f('filter')(newValue,{ status: false }).length;
-	},true);
-	
-	//當route改變，看你點的是All、Active或Completed，該選項會有框線
-	s.$on('$routeChangeSuccess', function () {
-		s.status = r.path;
-		//alert(s.status);
-	});
-}])
+todoAPP.controller('Ctrl1', [ '$scope', '$routeParams', '$filter',
+		function(s, r, f) {
+			s.myInputs = [];
+			s.inputString = '';
+			s.addtodo = function() {
+				s.myInputs.push({
+					text : s.inputString,
+					status : false
+				});
+				s.inputString = '';
+			};
+			s.remove = function(index) {
+				s.myInputs.splice(index, 1);
+			};
 
-todoAPP.controller('HideCtrl', ['$scope', '$routeParams',function(s,r){
-	s.isHide = function(inputStatus){
-		if(r.path == 'active'){
+			// 監控myInputs陣列元素的變化,第3個參數為true會呼叫equals方法來比較
+			s.$watch("myInputs", function(newValue, oldValue) {
+
+				s.count = f('filter')(newValue, {status : false}).length;
+				//如果陣列元素有未勾的選項，全選取消勾選
+				s.allChecked = !s.count;
+			}, true);
+
+			// 當route改變，看你點的是All、Active或Completed，該選項會有框線
+			s.$on('$routeChangeSuccess', function() {
+				s.status = r.path;
+			});
+			
+			s.markAll = function(status){
+				s.myInputs.forEach(function(elt, i, array) {
+					if(elt.status!=status){
+						elt.status = status;
+					}
+				})
+			}
+			
+		} ])
+
+todoAPP.controller('HideCtrl', [ '$scope', '$routeParams', function(s, r) {
+	s.isHide = function(inputStatus) {
+		if (r.path == 'active') {
 			return inputStatus;
-		}else if(r.path == 'completed'){
+		} else if (r.path == 'completed') {
 			return !inputStatus;
-		}
-		else
+		} else
 			return false;
 	};
-	
-}])
 
-
+} ])
