412743a7f6af7c79d78b28ee21a0467ce328712a
Hao.hung <eeit9306@gmail.com>
2017-07-29 13:49:59 +0800

    輸入後勾選選項，在點Active會把勾選的選項隱藏

--- todoMVC/WebContent/js/myModule.js
+++ todoMVC/WebContent/js/myModule.js
@@ -1,14 +1,31 @@
-var todoAPP = angular.module('TodoAPP',[]);
+var todoAPP = angular.module('TodoAPP',['ngRoute'],function(
+		$routeProvider) {
+	$routeProvider.when('/:path', {
+		templateUrl : 'view.html',
+		controller:'HideCtrl'
+	}).otherwise({
+		redirectTo : '/'
+	});
+});
 
 todoAPP.controller('Ctrl1',['$scope',function(s){
 	s.myInputs=[];
 	//s.classObj={useItalic: 'italic'};
 	s.inputString='';
 	s.addtodo = function() {
-		s.myInputs.push(s.inputString);
+		s.myInputs.push({text:s.inputString, status:false});
 		s.inputString='';
 	};
 	s.remove = function(index){
 		s.myInputs.splice(index, 1);
+	};
+}])
+
+todoAPP.controller('HideCtrl', ['$scope', '$routeParams',function(s,r){
+	s.isHide = function(status){
+		if(r.path == 'actives'){
+			return status;
+		}else
+			return false;
 	};
 }])
